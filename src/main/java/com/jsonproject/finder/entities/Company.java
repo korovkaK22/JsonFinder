260bc97bd55ae33944827ebd650edef754c4d506
Andrii Sereda <sa0959328930@gmail.com>
2024-04-16 14:19:53 +0300

    wrote entities' logic

--- src/main/java/com/jsonproject/finder/entities/Company.java
+++ src/main/java/com/jsonproject/finder/entities/Company.java
@@ -1,4 +1,17 @@
 package com.jsonproject.finder.entities;
 
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Company {
+ String name;
+ int employeesAmount;
+ int workingCarsAmount;
+
 }
--- src/main/java/com/jsonproject/finder/entities/TaxiDriver.java
+++ src/main/java/com/jsonproject/finder/entities/TaxiDriver.java
@@ -1,4 +1,19 @@
 package com.jsonproject.finder.entities;
 
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class TaxiDriver {
+    private String name;
+    private String company;
+    private int age;
+    private int drivingExperience;
+    private long salary;
+    private List<String> cars;
 }
