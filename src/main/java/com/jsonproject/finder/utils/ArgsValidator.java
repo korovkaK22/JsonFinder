ac2d96cacec17fb5622a4606294eb79aeb849334
Andrii Sereda <sa0959328930@gmail.com>
2024-04-18 06:08:50 +0300

    added the documentation in ArgsValidator.java

--- src/main/java/com/jsonproject/finder/utils/ArgsValidator.java
+++ src/main/java/com/jsonproject/finder/utils/ArgsValidator.java
@@ -7,9 +7,17 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class for validating arguments provided to the application.
+ * This class checks the validity of the directory paths and parameters specified by the user.
+ */
 public class ArgsValidator{
 
+    /**
+     * Stored valid parameter names based on the fields of the TaxiDriver class.
+     */
     List<String> fieldNames = new ArrayList<>();
+
 
     {
         Field[] fields = TaxiDriver.class.getDeclaredFields();
@@ -19,7 +27,12 @@ public class ArgsValidator{
     }
 
 
-
+    /**
+     * Validates the array of arguments provided to the application.
+     *
+     * @param args The command-line arguments to validate.
+     * @throws IllegalArgumentException if the number of arguments is not exactly 2, or if the arguments do not meet the specified criteria.
+     */
     public void validate(String[] args) throws IllegalArgumentException {
         if (args.length <2){
             throw new IllegalArgumentException(String.format("Total args is less than 2 (%d)",args.length));
@@ -33,6 +46,12 @@ public class ArgsValidator{
         validateParameter(args[1]);
     }
 
+    /**
+     * Validates that the specified parameter is a valid field name of the TaxiDriver class.
+     *
+     * @param parameter The parameter to validate.
+     * @throws IllegalArgumentException if the parameter does not match any field name in the TaxiDriver class.
+     */
      public void validateParameter(String parameter)  throws IllegalArgumentException {
 
         if (!fieldNames.contains(parameter.toLowerCase())) {
@@ -40,7 +59,12 @@ public class ArgsValidator{
         }
     }
 
-
+    /**
+     * Validates that the specified directory exists and is indeed a directory.
+     *
+     * @param dir The directory to validate.
+     * @throws IllegalArgumentException if the file is not a directory or does not exist.
+     */
     public void validatePath(File dir) throws IllegalArgumentException{
         if (!dir.exists()) {
             throw new IllegalArgumentException("Can't find such directory: " + dir);
