5c49d6d333f987ebc4f089ecf2cba060c2958da8
Andrii Sereda <sa0959328930@gmail.com>
2024-04-18 06:09:06 +0300

    added the documentation in DirectoryReader.java

--- src/main/java/com/jsonproject/finder/utils/DirectoryReader.java
+++ src/main/java/com/jsonproject/finder/utils/DirectoryReader.java
@@ -6,9 +6,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class DirectoryReader {
 
-    public static List<File> getAllFiles(File dirFile) {
+/**
+ * Utility class for reading directories and filtering out files based on specified criteria.
+ */
+public class DirectoryReader {
+    /**
+     * Retrieves all files from the specified directory that end with ".json".
+     *
+     * @param dirFile The directory from which files are to be listed.
+     * @return A list of File objects representing the JSON files found in the specified directory.
+     * @throws NullPointerException if the directory file object is null.
+     * @throws IllegalArgumentException if the directory does not exist or is not a directory.
+     */
+    public static List<File> getAllFiles(File dirFile) throws IllegalArgumentException, NullPointerException {
         validateDir(dirFile);
 
         FilenameFilter jsonFilter = (dir, name) -> name.toLowerCase().endsWith(".json");
@@ -16,7 +27,13 @@ public class DirectoryReader {
         return files != null ? new ArrayList<>(Arrays.asList(files)) : new ArrayList<>();
     }
 
-
+    /**
+     * Validates that the provided File object is a valid directory.
+     *
+     * @param dir The directory to validate.
+     * @throws NullPointerException if the directory reference is null.
+     * @throws IllegalArgumentException if the directory does not exist or is not actually a directory.
+     */
     private static void validateDir(File dir) throws IllegalArgumentException, NullPointerException{
         if (dir == null){
             throw new NullPointerException("This directory is null!");
