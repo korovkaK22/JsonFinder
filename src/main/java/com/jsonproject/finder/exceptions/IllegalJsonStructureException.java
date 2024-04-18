1da1d7b150879875b66d62f4d2fbe1fbefb8ab56
Andrii Sereda <sa0959328930@gmail.com>
2024-04-18 05:56:58 +0300

    Added documentations to IllegalJsonStructureException.java

--- src/main/java/com/jsonproject/finder/exceptions/IllegalJsonStructureException.java
+++ src/main/java/com/jsonproject/finder/exceptions/IllegalJsonStructureException.java
@@ -1,5 +1,11 @@
 package com.jsonproject.finder.exceptions;
 
+
+/**
+ * The IllegalJsonStructureException is a custom exception that signals an issue with the JSON file structure.
+ * This exception is typically thrown when a JSON file does not conform to the expected structure,
+ * such as when the JSON data is expected to be an array of objects but is not.
+ */
 public class IllegalJsonStructureException extends Exception{
     public IllegalJsonStructureException() {
     }
