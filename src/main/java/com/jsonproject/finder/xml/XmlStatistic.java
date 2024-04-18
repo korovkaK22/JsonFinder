cb2ac987aa99c93b17971507c2ae2159e8c17869
Andrii Sereda <sa0959328930@gmail.com>
2024-04-18 05:54:24 +0300

    Added documentations to XmlStatistic.java and XmlWriter.java classes

--- src/main/java/com/jsonproject/finder/xml/XmlStatistic.java
+++ src/main/java/com/jsonproject/finder/xml/XmlStatistic.java
@@ -1,6 +1,5 @@
 package com.jsonproject.finder.xml;
 import com.jsonproject.finder.statistic.Statistic;
-import com.jsonproject.finder.statistic.TextStatistic;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,32 +8,54 @@ import javax.xml.bind.annotation.*;
 import java.util.*;
 
 
+/**
+ * The XmlStatistic class represents a collection of statistical data that can be marshalled into XML format.
+ */
 @NoArgsConstructor
 @XmlRootElement(name = "statistics")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class XmlStatistic {
 
+    /**
+     * Stores statistic values in StatisticItem objects
+     */
     @XmlElement(name = "item")
     @Getter
     private final List<StatisticItem> items = new ArrayList<>();
 
-
+    /**
+     * Constructs an XmlStatistic object using data from a given Statistic instance.
+     * Converts each entry in the statistic map to an XML-serializable StatisticItem and sorts them.
+     *
+     * @param statistic the statistic object whose data will be converted to XML format.
+     */
     public XmlStatistic(Statistic statistic) {
         Map<String, Integer> stats = statistic.getStatistic();
         addAll(stats);
     }
 
+    /**
+     * Adds all entries from a map of statistics to the list of StatisticItems.
+     *
+     * @param stats a map of statistics where each key is a value and each value is the count of occurrences.
+     */
     public void addAll(Map<String, Integer> stats) {
         stats.forEach((key, value) -> items.add(new StatisticItem(key, value)));
         sortItems();
     }
 
-
+    /**
+     * Sorts the items in descending order based on the count of occurrences.
+     */
     public void sortItems(){
         Collections.sort(items);
     }
 
 
+    /**
+     * Inner class representing an item in the statistic. Each item is comparable based on its count,
+     * allowing for sorting then statistic by count.
+     */
     @XmlAccessorType(XmlAccessType.FIELD)
     @Getter
     @Setter
--- src/main/java/com/jsonproject/finder/xml/XmlWriter.java
+++ src/main/java/com/jsonproject/finder/xml/XmlWriter.java
@@ -1,20 +1,28 @@
 package com.jsonproject.finder.xml;
-
-import com.jsonproject.finder.threads.ThreadStatsCounter;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
 
+/**
+ * The XmlWriter class is responsible for writing XML data to a file. It takes an instance of XmlStatistic,
+ * which contains the statistics data, and writes this data to an XML file using JAXB for marshalling.
+ *
+ * @see XmlStatistic
+ */
 public class XmlWriter {
-    private static final Logger logger = LogManager.getLogger(XmlWriter.class);
     XmlStatistic xmlStatistic;
     Marshaller marshaller;
 
-    public XmlWriter(XmlStatistic xmlStatistic) throws JAXBException {
+    /**
+     * Constructs an XmlWriter with the specified XmlStatistic object.
+     * Initializes JAXB context and marshaller for the XmlStatistic class.
+     *
+     * @param xmlStatistic the XmlStatistic object containing the data to be written to XML.
+     * @throws JAXBException if an error occurs during the creation of JAXBContext or Marshaller.
+     * @throws NullPointerException if xmlStatistic is null.
+     */
+    public XmlWriter(XmlStatistic xmlStatistic) throws JAXBException, NullPointerException {
         if (xmlStatistic == null) {
             throw new NullPointerException("Statistic can't be null");
         }
@@ -25,6 +33,15 @@ public class XmlWriter {
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
     }
 
+
+    /**
+     * Writes the XML representation of the XmlStatistic object to the specified file.
+     *
+     * @param file the file to which the XML data will be written.
+     * @return true if the operation is successful.
+     * @throws JAXBException if an error occurs during the marshalling process.
+     * @throws NullPointerException if the file is null.
+     */
     public boolean writeXmlStatsToXml(File file) throws JAXBException {
         if (file == null) {
             throw new NullPointerException("Can't write statistic to null file");
@@ -32,6 +49,5 @@ public class XmlWriter {
 
         marshaller.marshal(xmlStatistic, file);
         return true;
-
     }
 }
